17e9b048c6a8ba12be0bf39c1dba8be91b6a32ec
Dereje Tessema <dtessema@myfuse1.education>
2025-06-02 09:50:19 -0400

    UI fix

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-import { blogPost } from "./data.js"
+import { blogPost } from "./data.js";
 
-const section = document.getElementById("section")
+const section = document.getElementById("section");
 
 function renderPosts() {
-    return blogPost.map(item => {
-        return `<div class="blog-items">
+  return blogPost
+    .map((item) => {
+      return `<div class="blog-items">
                 <img class="blog-img" src="${item.img}"/>
                 <p>${item.date}</p>
-                <h4>${item.title}<h4>
+                <h4>${item.title}</h4>
                 <p>${item.description}</p>
-        </div>`
-    }).join("")
+        </div>`;
+    })
+    .join("");
 }
-section.innerHTML = renderPosts()
+
+document.addEventListener("DOMContentLoaded", () => {
+  const section = document.getElementById("section");
+  section.innerHTML = renderPosts();
+});
